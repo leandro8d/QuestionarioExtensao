824c85d009246589ebc6f7cdf12b3f619917befe
Jonathan <jlima.tecinfo@gmail.com>
2017-11-04 01:16:13 -0200

    Finalização dos questionários. Atualização do routes.js

--- web/scripts/configs/routes.js
+++ web/scripts/configs/routes.js
@@ -11,32 +11,24 @@ app.config(function($routeProvider) {
         templateUrl : "./views/formulario/questao2.html",
         controller: "formularioController"
     })
-	.when("/formulario3", {
+	.when("/questao3", {
         templateUrl : "./views/formulario/questao3.html",
         controller: "formularioController"
     })
-	.when("/formulario4", {
+	.when("/questao4", {
         templateUrl : "./views/formulario/questao4.html",
         controller: "formularioController"
     })
-	.when("/formulario5", {
+	.when("/questao5", {
         templateUrl : "./views/formulario/questao5.html",
         controller: "formularioController"
     })
-	.when("/formulario6", {
+	.when("/questao6", {
         templateUrl : "./views/formulario/questao6.html",
         controller: "formularioController"
     })
-	.when("/formulario7", {
+	.when("/questao7", {
         templateUrl : "./views/formulario/questao7.html",
         controller: "formularioController"
-    })
-	.when("/formulario8", {
-        templateUrl : "./views/formulario/questao8.html",
-        controller: "formularioController"
-    })
-	.when("/formulario9", {
-        templateUrl : "./views/formulario/questao9.html",
-        controller: "formularioController"
-    })
+    
 });
