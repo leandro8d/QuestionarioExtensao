d15e3a80e3ba2044b2e0dc86f408a5d949837450
Jonathan <jlima.tecinfo@gmail.com>
2017-11-04 00:24:31 -0200

    Adição dos Questionários

--- web/scripts/configs/routes.js
+++ web/scripts/configs/routes.js
@@ -3,8 +3,40 @@ app.config(function($routeProvider) {
     .when("/", {
         templateUrl : "./views/home/home.html",
         controller: "homeController"
-    }).when("/formulario", {
-        templateUrl : "./views/formulario/formulario.html",
+    }).when("/questao1",{
+        templateUrl : "./views/formulario/questao1.html",
         controller: "formularioController"
-    });
+	})
+	.when("/questao2", {
+        templateUrl : "./views/formulario/questao2.html",
+        controller: "formularioController"
+    })
+	.when("/formulario3", {
+        templateUrl : "./views/formulario/questao3.html",
+        controller: "formularioController"
+    })
+	.when("/formulario4", {
+        templateUrl : "./views/formulario/questao4.html",
+        controller: "formularioController"
+    })
+	.when("/formulario5", {
+        templateUrl : "./views/formulario/questao5.html",
+        controller: "formularioController"
+    })
+	.when("/formulario6", {
+        templateUrl : "./views/formulario/questao6.html",
+        controller: "formularioController"
+    })
+	.when("/formulario7", {
+        templateUrl : "./views/formulario/questao7.html",
+        controller: "formularioController"
+    })
+	.when("/formulario8", {
+        templateUrl : "./views/formulario/questao8.html",
+        controller: "formularioController"
+    })
+	.when("/formulario9", {
+        templateUrl : "./views/formulario/questao9.html",
+        controller: "formularioController"
+    })
 });
