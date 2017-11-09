0ecfbb014161ffcfe9202157261be559bfa20228
Jonathan <jlima.tecinfo@gmail.com>
2017-11-09 20:22:39 -0200

    Atualização do questionários 1 e do script de rotas!

--- web/scripts/configs/routes.js
+++ web/scripts/configs/routes.js
@@ -7,28 +7,6 @@ app.config(function($routeProvider) {
         templateUrl : "./views/formulario/questao1.html",
         controller: "formularioController"
 	})
-	.when("/questao2", {
-        templateUrl : "./views/formulario/questao2.html",
-        controller: "formularioController"
-    })
-	.when("/questao3", {
-        templateUrl : "./views/formulario/questao3.html",
-        controller: "formularioController"
-    })
-	.when("/questao4", {
-        templateUrl : "./views/formulario/questao4.html",
-        controller: "formularioController"
-    })
-	.when("/questao5", {
-        templateUrl : "./views/formulario/questao5.html",
-        controller: "formularioController"
-    })
-	.when("/questao6", {
-        templateUrl : "./views/formulario/questao6.html",
-        controller: "formularioController"
-    })
-	.when("/questao7", {
-        templateUrl : "./views/formulario/questao7.html",
-        controller: "formularioController"
+	
     
 });
